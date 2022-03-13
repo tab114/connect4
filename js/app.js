147f992bf57023613cc71834af3afefa04f92b3b
lefteris <lefteris.nikolidakis@telegraph.co.uk>
2022-03-13 05:00:01 +0000

    created game

---
+++ js/app.js
@@ -0,0 +1,17 @@
+const game = new Game();
+
+/** 
+ * Listens for click on `#begin-game` and calls startGame() on game object
+ */
+document.getElementById('begin-game').addEventListener('click', (event) => {
+    game.startGame();
+    event.target.style.display = 'none';
+    document.getElementById('play-area').style.opacity = '1';
+});
+
+/** 
+ * Listen for keyboard presses
+ */
+document.addEventListener('keydown', (event) => {
+    game.handleKeydown(event);
+});
